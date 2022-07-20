8731718e31c1c22ea01292b7b0568cc6422b1fa1
jrbreeden <jrbreeden55@gmail.com>
2022-07-20 18:03:37 -0400

    Added two birds

---
+++ birdcollector/main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Bird
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def birds_index(request):
+    birds = Bird.objects.all()
+    return render(request, 'birds/index.html', { 'birds': birds })
