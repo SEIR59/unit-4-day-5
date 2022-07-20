82815304ed77d3de721a53db87a556d6f37d5128
jrbreeden <jrbreeden55@gmail.com>
2022-07-20 18:58:39 -0400

    Update and Delete birds work

--- birdcollector/main_app/models.py
+++ birdcollector/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Bird(models.Model):
@@ -7,5 +8,8 @@ class Bird(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
 
-def __str__(self):
+  def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'bird_id': self.id})
